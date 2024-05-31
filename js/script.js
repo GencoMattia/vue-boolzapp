323e24b535fff320fba4aaf3a2ac234fbbf96477
GencoMattia <gencomattia@hotmail.it>
2024-05-31 15:35:30 +0200

    HTML: added div for message-received and sent. JS: added activeChat data

--- js/script.js
+++ js/script.js
@@ -174,7 +174,9 @@ createApp({
                 messages: [
                     
                 ], 
-            }
+            },
+
+            activeChat: 0,
         }
     }
 }).mount('#app')
